c816a8f08d20daf39b5c33da611c151ce34c3b0d
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-02-18 20:41:46 -0800

    Completed Stack challenge, all tests passing

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,27 @@
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.append('/Users/israelgonzalez/Documents/lambda/cs/Data-Structures/doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 class Stack:
     def __init__(self):
-        self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
-
+        self.storage = DoublyLinkedList()
+        self.size = self.storage.length
+    # We'll make the tail the part of the list where items are popped off from
     def push(self, value):
+        self.storage.add_to_tail(value)
         pass
 
     def pop(self):
-        pass
+        if self.storage.length > 0:
+            temp_tail = self.storage.tail
+            self.storage.remove_from_tail()
+            return temp_tail.value
+        else:
+            return None
+        
 
     def len(self):
+        return self.storage.length
         pass
